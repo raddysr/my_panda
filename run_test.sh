7ad8e8deb21a511c97999fec09c186262a3e275f
Radi Radev <33155474+raddysr@users.noreply.github.com>
2021-05-23 10:28:05 +0300

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
-pytest tests/test_dataframe.py::TestDataFrameCreation::$1        
+pytest tests/test_dataframe.py::TestDataFrameCreation::$1  
 
+
+# Run this file with test_<function name> as a argument
