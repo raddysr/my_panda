b2145326c26120672e6a8960f27a090fb360c8b2
raddysr <raddy.radev@gmail.com>
2021-05-24 12:05:01 +0300

    add CI/CD tools

--- radanda/__init__.py
+++ radanda/__init__.py
@@ -1,3 +1,4 @@
+from typing import Type
 import numpy as np
 
 __version__ = '0.0.1'
@@ -29,7 +30,7 @@ class DataFrame:
                 raise TypeError('value must be numpy array')
             if value.ndim != 1:
                 raise ValueError('value must be one dimensional')
-    
+        
 
 
 
@@ -54,6 +55,19 @@ class DataFrame:
     def columns(self):
         return(list(self._data))
 
+    @columns.setter
+    def columns(self, columns):
+        if not isinstance(columns, list):
+            raise TypeError('`columns` must be a list')
+        if len(columns) != len(self._data):
+            raise ValueError('`new` colums must be equal length with as df')
+        for column in columns:
+            if not isinstance(column, str):
+                raise TypeError('`col` must be a string')
+        if len(columns) != len(set(columns)):
+            raise ValueError('Your column have duplicates')
+        self._data = dict(zip(columns,self._data.values()))
+
     def __len__(self):
         return len(next(iter(self._data.values())))
     
@@ -63,6 +77,112 @@ class DataFrame:
     def add_docs(sefl):
         pass
 
+    @property
+    def shape(self):
+        return len(self), len(self._data)
+
+
+    def _repr_html_(self):
+        """
+        Used to create a string of HTML to nicely display the DataFrame
+        in a Jupyter Notebook. Different string formatting is used for
+        different data types.
+
+        The structure of the HTML is as follows:
+        <table>
+            <thead>
+                <tr>
+                    <th>data</th>
+                    ...
+                    <th>data</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><strong>{i}</strong></td>
+                    <td>data</td>
+                    ...
+                    <td>data</td>
+                </tr>
+                ...
+                <tr>
+                    <td><strong>{i}</strong></td>
+                    <td>data</td>
+                    ...
+                    <td>data</td>
+                </tr>
+            </tbody>
+        </table>
+        """
+
+        html = '<table><thead><tr><th></th>'
+        for col in self.columns:
+            html += f"<th>{col:10}</th>"
+
+        html += '</tr></thead>'
+        html += "<tbody>"
+
+        only_head = False
+        num_head = 10
+        num_tail = 10
+        if len(self) <= 20:
+            only_head = True
+            num_head = len(self)
+
+        for i in range(num_head):
+            html += f'<tr><td><strong>{i}</strong></td>'
+            for col, values in self._data.items():
+                kind = values.dtype.kind
+                if kind == 'f':
+                    html += f'<td>{values[i]:10.3f}</td>'
+                elif kind == 'b':
+                    html += f'<td>{values[i]}</td>'
+                elif kind == 'O':
+                    v = values[i]
+                    if v is None:
+                        v = 'None'
+                    html += f'<td>{v:10}</td>'
+                else:
+                    html += f'<td>{values[i]:10}</td>'
+            html += '</tr>'
+
+        if not only_head:
+            html += '<tr><strong><td>...</td></strong>'
+            for i in range(len(self.columns)):
+                html += '<td>...</td>'
+            html += '</tr>'
+            for i in range(-num_tail, 0):
+                html += f'<tr><td><strong>{len(self) + i}</strong></td>'
+                for col, values in self._data.items():
+                    kind = values.dtype.kind
+                    if kind == 'f':
+                        html += f'<td>{values[i]:10.3f}</td>'
+                    elif kind == 'b':
+                        html += f'<td>{values[i]}</td>'
+                    elif kind == 'O':
+                        v = values[i]
+                        if v is None:
+                            v = 'None'
+                        html += f'<td>{v:10}</td>'
+                    else:
+                        html += f'<td>{values[i]:10}</td>'
+                html += '</tr>'
+
+        html += '</tbody></table>'
+        return html
+
+    @property
+    def values(self):
+        return np.column_stack(list(self._data.values()))
+
+    @property
+    def dtypes(self):
+        DTYPE_NAME = {'O':'string', 'i':'int', 'f':'float', 'b':'bool'}
+        col_names = np.array(list(self._data.keys()))
+        dtypes = [DTYPE_NAME[value.dtype.kind] for value in self._data.values()]
+        dtypes = np.array(dtypes)
+        new_data = {'Column Name': col_names, 'Data Type': dtypes}
+        return DataFrame(new_data)
 
 def read_csv(fn):
     """ 
@@ -76,4 +196,3 @@ def read_csv(fn):
     -------
     A DataFrame
     """
-    pass
